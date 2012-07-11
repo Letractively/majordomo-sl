01e735651a773fb5207c39c6169faeabeff75ac4
sergejey <sergejey@gmail.com>
2012-07-11 10:00:03 +0000

    minor fix for playmedia() with VLC player

--- modules/app_player/app_player.class.php
+++ modules/app_player/app_player.class.php
@@ -222,8 +222,9 @@ function usual(&$out) {
 
       if ($command=='refresh') {
        $out['PLAY']=preg_replace('/\\\\$/is', '', $out['PLAY']);
-       curl_setopt($ch, CURLOPT_URL, "http://".$terminal['HOST'].":".$terminal['PLAYER_PORT']."/rc/?command=vlc_play&param=".urlencode(''.str_replace('/', "\\", ($out['PLAY'])).''));
-       //echo "http://".$terminal['HOST']."/rc/?command=vlc_play&param=".urlencode(''.str_replace('/', "\\", utf2win($out['PLAY'])).'');
+       $out['PLAY']=preg_replace('/\/$/is', '', $out['PLAY']);
+       $path=urlencode(''.str_replace('/', "\\", ($out['PLAY'])));
+       curl_setopt($ch, CURLOPT_URL, "http://".$terminal['HOST'].":".$terminal['PLAYER_PORT']."/rc/?command=vlc_play&param=".$path);
        $res=curl_exec($ch);
       }
 
